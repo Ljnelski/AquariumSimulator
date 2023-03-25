14edee48dde330d77e57d547b272bdcca14a050f
Atta Jirofty <jirofty76@gmail.com>
2023-03-24 23:48:45 -0400

    Added Crossfade effect - UI enhdance

--- Assets/_Scripts/Main Menu/MainMenu.cs
+++ Assets/_Scripts/Main Menu/MainMenu.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Animator transition;
+    [SerializeField] float transitionTime = 1f;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelindex)
+    {
+        // Play anim
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load Scene
+        SceneManager.LoadScene(levelindex);
     }
 
     public void ExitGame()
@@ -14,5 +28,6 @@ public class MainMenu : MonoBehaviour
         Debug.Log("EXIT BUTTON CLICKED!");
         Application.Quit();
     }
+
 
 }
