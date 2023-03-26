342cd30188f8e265bf5cf9a7df69fd1ebfa3ddbb
Ljnelski <Ljnelski@gmail.com>
2023-03-25 20:48:20 -0400

    Added GameState Singleton, Reorganized Project

---
+++ Assets/_Scripts/UI/AquariumParameterData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO Set up with all of systems later to neatly integrate with UI
+public class AquariumParameterData : ScriptableObject
+{
+    public string Name;
+    public string ChemicalSymbol;
+}
