9882d50bd13ed986655ca665d0fd0e62859eea09
Atta Jirofty <jirofty76@gmail.com>
2023-04-07 00:39:11 -0400

    Added Betta Fish Params as Comments

--- Assets/_Scripts/Aquarium/Fish/Fish.cs
+++ Assets/_Scripts/Aquarium/Fish/Fish.cs
@@ -7,14 +7,14 @@ public class Fish : AquariumObject
     [SerializeField] private float foodConsumeRate = 1f;
     [SerializeField] private float oxygenConsumeRate = 1f;
 
-    [SerializeField] private float health = 100f; // Range from 0-100 ( float ? )
+    [SerializeField] private float health = 100f; // Range from 0-100 
 
-    [SerializeField] private float hungerAmount = 0; // Range from ?-?
-    [SerializeField] private float hungerIncreaseRate = 0; // By what rate the fish gets hungry
-    
-    [SerializeField] private float requiredPh = 0; // Range from 0-14
+    [SerializeField] private float hungerAmount = 0; // Range from 0-100 
+    [SerializeField] private float hungerIncreaseRate = 0; // By what rate the fish gets hungry -- should be fed once or twice a day, with each feeding consisting of 2-3 pellets or flakes
 
-    [SerializeField] private float requiredTemperature = 0; // celsius
+    [SerializeField] private float requiredPh = 0; // Range from 0-14 -- For Betta fish, the ph should be : 6.5 - 7.5 
+
+    [SerializeField] private float requiredTemperature = 0; // celsius -- For Betta fish, the temp should be 24C-28C
 
     //float happinessAmount = 100; // Range from 0-100 ( float? )
 
