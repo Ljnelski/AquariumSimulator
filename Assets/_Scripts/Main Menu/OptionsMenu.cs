bd293281a218adb0bcda51b829e49c479c816ee5
Ljnelski <Ljnelski@gmail.com>
2023-04-17 21:56:06 -0400

    More quick fixes for build

--- Assets/_Scripts/Main Menu/OptionsMenu.cs
+++ Assets/_Scripts/Main Menu/OptionsMenu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
-using TMPro.EditorUtilities;
 using TMPro;
 
 public class OptionsMenu : MonoBehaviour
